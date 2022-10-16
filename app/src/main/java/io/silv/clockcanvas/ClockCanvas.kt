04e980a340231c8e41eb4af9537a6cb38afac1bb
David Silver <david.silver232@gmail.com>
2022-10-16 12:29:09 -0400

    added text + fixed clock

--- app/src/main/java/io/silv/clockcanvas/ClockCanvas.kt
+++ app/src/main/java/io/silv/clockcanvas/ClockCanvas.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -54,108 +55,107 @@ fun CanvasClock(
     val radius = clockStyle.clockRadius
     Canvas(modifier = modifier.border(12.dp, Color.Black)) {
         center = Offset(this.center.x, this.center.y)
-        drawContext.canvas.nativeCanvas.apply {
-            drawCircle(
-                center.x,
-                center.y,
-                radius.toPx(),
-                Paint().apply {
-                    0
-                    color = android.graphics.Color.WHITE
-                    setStyle(Paint.Style.FILL)
-                    setShadowLayer(
-                        60f,
-                        0f,
-                        0f,
-                        android.graphics.Color.argb(50, 0, 0, 0)
-                    )
+
+        rotate(270f) {
+            drawContext.canvas.nativeCanvas.apply {
+                drawCircle(
+                    center.x,
+                    center.y,
+                    radius.toPx(),
+                    Paint().apply {
+                        0
+                        color = android.graphics.Color.WHITE
+                        setStyle(Paint.Style.FILL)
+                        setShadowLayer(
+                            60f,
+                            0f,
+                            0f,
+                            android.graphics.Color.argb(50, 0, 0, 0)
+                        )
+                    }
+                )
+            }
+            //draw line
+            //Formula for radians
+            //x = radius * cos(angle in radians)
+            //y = radius * sin(angle in radians)
+            for (i in 1..60) {
+                val lineType = when (i % 5 == 0) {
+                    true -> LineType.ThirtyMin
+                    else -> LineType.FiveMin
                 }
-            )
-        }
-        //draw line
-        //Formula for radians
-        //x = radius * cos(angle in radians)
-        //y = radius * sin(angle in radians)
-        for (i in 1..60) {
-            val lineType = when(i % 5 == 0) {
-                true -> LineType.ThirtyMin
-                else-> LineType.FiveMin
+                val lineLength = when (lineType) {
+                    is LineType.ThirtyMin -> clockStyle.thirtyMinLineLength.toPx()
+                    else -> clockStyle.fiveMinLineLength.toPx()
+                }
+                val lineWidth = when (lineType) {
+                    is LineType.ThirtyMin -> 1.5.dp
+                    else -> 0.5.dp
+                }
+                val lineColor = Color.Black
+                val lineStart = Offset(
+                    x = radius.toPx() * cos((i * 6).toFloat().toRadians()) + center.x,
+                    y = radius.toPx() * sin((i * 6).toFloat().toRadians()) + center.y
+                )
+                val lineEnd = Offset(
+                    x = (radius.toPx() - lineLength) * cos(
+                        (i * 6).toFloat().toRadians()
+                    ) + center.x,
+                    y = (radius.toPx() - lineLength) * sin(
+                        (i * 6).toFloat().toRadians()
+                    ) + center.y,
+                )
+                drawLine(
+                    lineColor,
+                    lineStart,
+                    lineEnd,
+                    lineWidth.toPx()
+                )
             }
-            val lineLength = when(lineType) {
-                is LineType.ThirtyMin -> clockStyle.thirtyMinLineLength.toPx()
-                else -> clockStyle.fiveMinLineLength.toPx()
-            }
-            val lineWidth = when(lineType)  {
-                is LineType.ThirtyMin -> 1.5.dp
-                else -> 0.5.dp
-            }
-            val lineColor = Color.Black
-            val lineStart = Offset(
-                x = radius.toPx()  * cos((i * 6).toFloat().toRadians()) + center.x,
-                y = radius.toPx()  * sin((i * 6).toFloat().toRadians()) + center.y
-            )
-            val lineEnd = Offset(
-                x = (radius.toPx()  - lineLength) * cos((i * 6).toFloat().toRadians()) + center.x,
-                y = (radius.toPx() - lineLength) * sin((i * 6).toFloat().toRadians())+ center.y,
-            )
+
+            //draw Hour Min Second hands
+            //seconds
+            //x = radius * cos(angle in radians)
+            //y = radius * sin(angle in radians)
             drawLine(
-                lineColor,
-                lineStart,
-                lineEnd,
-                lineWidth.toPx()
+                color = clockStyle.secondHandColor,
+                start = Offset(
+                    center.x,
+                    center.y
+                ),
+                end = Offset(
+                    x = clockStyle.secondHandLength.toPx() * cos((seconds * 6f).toRadians()) + center.x,
+                    y = clockStyle.secondHandLength.toPx() * sin((seconds * 6f).toRadians()) + center.y
+                ),
+                strokeWidth = 2.dp.toPx()
+            )
+            //minutes
+            drawLine(
+                color = clockStyle.minHandColor,
+                start = Offset(
+                    center.x,
+                    center.y
+                ),
+                end = Offset(
+                    x = clockStyle.minHandLength.toPx() * cos((minutes * 6f).toRadians()) + center.x,
+                    y = clockStyle.minHandLength.toPx() * sin((minutes * 6f).toRadians()) + center.y
+                ),
+                strokeWidth = 3.dp.toPx()
+            )
+            //hours
+            drawLine(
+                color = clockStyle.hourHandColor,
+                start = Offset(
+                    center.x,
+                    center.y
+                ),
+                end = Offset(
+                    x = clockStyle.hourHandLength.toPx() * cos((hours * 30f).toRadians()) + center.x,
+                    y = clockStyle.hourHandLength.toPx() * sin((hours * 30f).toRadians()) + center.y
+                ),
+                strokeWidth = 3.5.dp.toPx()
             )
         }
-
-        //draw Hour Min Second hands
-        //seconds
-        //x = radius * cos(angle in radians)
-        //y = radius * sin(angle in radians)
-        drawLine(
-            color = clockStyle.secondHandColor,
-            start = Offset(
-                center.x,
-                center.y
-            ),
-            end = Offset(
-                x = clockStyle.secondHandLength.toPx() * cos((seconds * 6f).toRadians()) + center.x,
-                y = clockStyle.secondHandLength.toPx() * sin((seconds * 6f).toRadians()) + center.y
-            ),
-            strokeWidth = 2.dp.toPx()
-        )
-        //minutes
-        drawLine(
-            color = clockStyle.minHandColor,
-            start = Offset(
-                center.x,
-                center.y
-            ),
-            end = Offset(
-                x = clockStyle.minHandLength.toPx() * cos((minutes * 0.25f).toRadians()) + center.x,
-                y = clockStyle.minHandLength.toPx() * sin((minutes * 0.25f).toRadians()) + center.y
-            ),
-            strokeWidth = 3.dp.toPx()
-        )
-        //hours
-        drawLine(
-            color = clockStyle.hourHandColor,
-            start = Offset(
-                center.x,
-                center.y
-            ),
-            end = Offset(
-                x = clockStyle.hourHandLength.toPx() * cos((hours* 30f).toRadians()) + center.x,
-                y = clockStyle.hourHandLength.toPx() * sin((hours * 030f).toRadians()) + center.y
-            ),
-            strokeWidth = 3.5.dp.toPx()
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun previewCanvasClock() {
-    ClockCanvasTheme {
-
     }
 }
 
--- app/src/main/java/io/silv/clockcanvas/MainActivity.kt
+++ app/src/main/java/io/silv/clockcanvas/MainActivity.kt
@@ -6,24 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.silv.clockcanvas.ui.theme.ClockCanvasTheme
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
@@ -32,7 +27,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ClockCanvasTheme {
-                val model: MyViewModel by viewModels()
+                val viewModel: MyViewModel by viewModels()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -43,37 +38,30 @@ class MainActivity : ComponentActivity() {
                     }
                     val coroutineScope = rememberCoroutineScope()
                     LaunchedEffect(key1 = true) {
-                        model.time.collectLatest {
+                        viewModel.time.collectLatest {
                             time = it
-                            delay(500)
                         }
                     }
-                    Box(modifier = Modifier.fillMaxSize()) {
-                        CanvasClock(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(400.dp),
-                            hours = time.hour,
-                            minutes = time.minute,
-                            seconds = time.second
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Box {
+                            CanvasClock(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(400.dp),
+                                hours = time.hour,
+                                minutes = time.minute,
+                                seconds = time.second
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            text = "${time.hour} : ${time.minute} : ${time.second}",
+                            fontSize = 36.sp,
+                            fontWeight = FontWeight.SemiBold,
                         )
                     }
-
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ClockCanvasTheme {
-        Greeting("Android")
     }
 }
